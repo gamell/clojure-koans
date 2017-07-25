cae5e0e950cbd9aacb688e9d6dfcee6911016391
Joan Gamell <gamell@@users.noreply.github.com>
2017-07-25 07:50:10 -0700

    14 and part of 15

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -10,14 +10,22 @@
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      false
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [coll coll
+         res []]
+      (if (empty? coll)
+        res
+        (recur (rest coll) (cons (first coll) res)))))
 
 (defn factorial [n]
-  __)
+    (loop [n n
+           acc 1]
+        (if (zero? n)
+          acc
+          (recur (dec n) (* acc n)))))
 
 (meditations
   "Recursion ends with a base case"
